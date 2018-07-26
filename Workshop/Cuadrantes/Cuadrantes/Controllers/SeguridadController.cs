23529ee227108b9c74c45a6b4d5e5a76093618b1
Eduar Ojeda <eduar.ojeda2220@correo.policia.gov.co>
2018-07-26 07:25:09 -0500

    Nuevos Cambios

--- Workshop/Cuadrantes/Cuadrantes/Controllers/SeguridadController.cs
+++ Workshop/Cuadrantes/Cuadrantes/Controllers/SeguridadController.cs
@@ -9,20 +9,22 @@ namespace Cuadrantes.Controllers
     [Route("api/[controller]")]
     public class SeguridadController : Controller
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <param name="clave"></param>
+        /// <returns></returns>
         [HttpPost]
         public string IniciarSesion(string usuario, string clave)
         {
-            if (usuario == null && clave == null)
-            {
-                return "Datos invalidos, ingrese nuevamente los datos";
-            }
-            return "Bienvenido !!!";
+            return string.Empty;
         }
         [HttpPost]
         public string Registro(string cedula, DateTime fechaExpedicion,
             string telefono, string correo)
         {
-            return "Usuario registrado";
+            return string.Empty;
         }
     }
 }
